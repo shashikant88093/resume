896845331f21ee182637e4e8bff2a59da18ad82e
shashikant88093 <shashi94goswami@gmail.com>
2019-07-28 19:39:30 +0530

    Updates

---
+++ precache-manifest.3d325aa4a53a35af4bac28d72e5aeb06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf2646a592505c8c3d29f9942333240",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "81b4de7fa954a19777c4",
+    "url": "/resume/static/css/main.12dbd4db.chunk.css"
+  },
+  {
+    "revision": "6018c684d110afdf8c90",
+    "url": "/resume/static/js/2.67a3edcb.chunk.js"
+  },
+  {
+    "revision": "81b4de7fa954a19777c4",
+    "url": "/resume/static/js/main.7ce99ef8.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  }
+]);
